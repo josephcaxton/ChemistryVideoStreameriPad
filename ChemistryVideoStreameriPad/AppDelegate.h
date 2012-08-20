5b2e86fbc76e48f633237629e14b05b33ee177f9
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-08-20 11:35:32 +0100

    Initial Commit

---
+++ ChemistryVideoStreameriPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChemistryVideoStreameriPad
+//
+//  Created by Joseph caxton-Idowu on 20/08/2012.
+//  Copyright (c) 2012 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
