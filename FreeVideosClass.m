67e71ab8dccaa7a9496914876ab9a15d2373bbeb
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2013-03-22 11:30:16 +0000

    last

--- FreeVideosClass.m
+++ FreeVideosClass.m
@@ -263,12 +263,14 @@
 }
 
 
--(BOOL)ShouldIDownloadOrNot:(NSString*)urllPath:(NSString*)LocalFileLocation{
+-(BOOL)ShouldIDownloadOrNot:(NSString*) urllPath :(NSString*)LocalFileLocation{
     
     AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
-   BOOL ReturnVal =  [appDelegate downloadFileIfUpdated:urllPath:LocalFileLocation];
+    BOOL ReturnVal =  [appDelegate downloadfileifUpdated:urllPath location:LocalFileLocation];
     
     return ReturnVal;
+    
+
    
     
     
@@ -349,7 +351,7 @@
             obj.Free = NO; 
         }
         obj.Subject = Subject;
-        obj.M3u8 = M3u8;
+        obj.M3u8 = [M3u8 stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
         
         if ([Sociallyfree isEqualToString: @"1"]){
             
